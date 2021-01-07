c62985c5f957bf944302e7036721da1075de2c1c
qw <123@163.com>
2021-01-07 15:46:48 +0800

    轮播图

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,38 +1,18 @@
-//Page Object
 Page({
   data: {
-    
+    swiperList: [],
   },
-  //options(Object)
-  onLoad: function(options){
-    
+  onLoad: function () {
+    const that = this;
+    wx.request({
+      url:
+        "https://www.fastmock.site/mock/afa8f2b5a50c33249b6143fcebcfd5e2/wx/api/swiperdata",
+      success(res) {
+        that.setData({
+          swiperList: res.data.message || [],
+        });
+        console.log(res);
+      },
+    });
   },
-  onReady: function(){
-    
-  },
-  onShow: function(){
-    
-  },
-  onHide: function(){
-
-  },
-  onUnload: function(){
-
-  },
-  onPullDownRefresh: function(){
-
-  },
-  onReachBottom: function(){
-
-  },
-  onShareAppMessage: function(){
-
-  },
-  onPageScroll: function(){
-
-  },
-  //item(index,pagePath,text)
-  onTabItemTap:function(item){
-
-  }
 });
