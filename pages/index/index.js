89ec517e3d001c719b1a1b4c9a03ad90519dfdb4
qw <123@163.com>
2021-01-07 17:00:20 +0800

    导航的布局

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,14 +2,30 @@ import { request } from "../../request/index.js";
 Page({
   data: {
     swiperList: [],
+    navigateList: [],
   },
   onLoad: function () {
+    this.fetchSwiperData();
+    this.fetchNavigateData();
+  },
+
+  fetchSwiperData: function () {
     request({
       url:
         "https://www.fastmock.site/mock/afa8f2b5a50c33249b6143fcebcfd5e2/wx/api/swiperdata",
     }).then((res) => {
       this.setData({
         swiperList: res.data.message || [],
+      });
+    });
+  },
+  fetchNavigateData: function () {
+    request({
+      url:
+        "https://www.fastmock.site/mock/afa8f2b5a50c33249b6143fcebcfd5e2/wx/api/navigation",
+    }).then((res) => {
+      this.setData({
+        navigateList: res.data.list || [],
       });
     });
   },
