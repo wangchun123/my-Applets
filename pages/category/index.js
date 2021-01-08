93b7c4e99c8c9637e256fd66f4b2c143f48ee459
qw <123@163.com>
2021-01-08 16:43:09 +0800

    分类界面编写

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,23 @@
-// pages/category/index.js
+import { request } from "../../request/index.js";
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    leftList: [],
+    rightList: [],
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  categyList: [],
+  onLoad: function () {
+    this.fetchCategyData();
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  fetchCategyData: function () {
+    request({ url: "/category" }).then((res) => {
+      this.categyList = res.data.list;
+      const leftList = this.categyList.map((item) => item.name);
+      const rightList = this.categyList[0].children;
+      this.setData({
+        leftList,
+        rightList,
+      });
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-})
+});
