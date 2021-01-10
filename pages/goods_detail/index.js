395403a8f3363032407099c1afa10e54ea46d116
qw <123@163.com>
2021-01-10 19:44:32 +0800

    商品详情

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,66 +1,29 @@
-// pages/goods_detail/index.js
+import { request } from "../../request/index.js";
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    detailList: {},
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  bigImage: {},
   onLoad: function (options) {
-
+    this.fetchDetailData();
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  fetchDetailData: function () {
+    request({
+      url: "/goods/detail",
+    }).then((res) => {
+      const { list } = res.data;
+      this.bigImage = list;
+      this.setData({
+        detailList: list,
+      });
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  itemClick: function (e) {
+    const { index } = e.currentTarget.dataset;
+    const url = this.bigImage.pic.map((item) => item.img_src);
+    wx.previewImage({
+      current: url[index],
+      urls: url,
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-})
+});
